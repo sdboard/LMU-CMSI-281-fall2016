3790f4b97ebc35ebef739b252f5c4a04c4d446b6
Stephen Board <sboard@lion.lmu.edu>
2016-10-07 15:28:47 -0700

    I was closing out of documents and I saved this file before closing it.
    This commit likely contains an added Node class added in the last few minutes of class not as reuirement, but to explore the contructor for Node

---
+++ Stack.java
@@ -0,0 +1,40 @@
+class Stack{
+    class Node{
+        String item;
+        Node next;
+
+        public Node(){
+            item = null;
+            next = null;
+        }
+
+        public Node(String word){
+            item = word;
+            next = null;
+        }
+    }
+    private Node first;
+    private int count;
+
+    public Stack(){
+        first = null;
+        count = 0;
+    }
+
+    public void push(String item){
+        Node newNode = new Node(item);
+        newNode = first;
+        first = newNode;
+    }
+
+    public String pop(){
+        String item = first.item;
+        first = first.next;
+        return item;
+
+    }
+
+    public String peek(){
+        return first.item;
+    }
+}
