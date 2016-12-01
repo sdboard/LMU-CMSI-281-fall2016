4fec90edf4a0b12ec5ae2f57dafddd2f0ce7beac
Stephen Board <sboard@lion.lmu.edu>
2016-11-30 23:02:46 -0800

    Broke the ice with this fun with trees project :D

---
+++ FunWithTrees/SDBTrie.java
@@ -0,0 +1,23 @@
+//SDBTrie.java
+public class SDBTrie{
+	Node root;
+
+	public SDBTrie(){
+		root = null;
+	}
+
+	class Node {
+		char c;
+		Node[] children;
+
+		public Node(){
+			c = '';
+
+		}
+
+		public Node(char a);
+			c = a;
+			
+
+	}
+}
