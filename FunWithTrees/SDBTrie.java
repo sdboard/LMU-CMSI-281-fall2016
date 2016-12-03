b9191f7008c02ca3483d1765ce137a3d4e174e18
Stephen Board <sboard@lion.lmu.edu>
2016-12-02 23:44:24 -0800

    I added some methods to the interface I copied some code from earlier this year to input the words I started on the add method and made a helper method. I started exploring what overloaded Node constructor I might need. :D

--- FunWithTrees/SDBTrie.java
+++ FunWithTrees/SDBTrie.java
@@ -1,17 +1,59 @@
 //SDBTrie.java
-public class SDBTrie{
+import java.io.*;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class SDBTrie implements TrieInterface {
+	Node pointer;
 	Node root;
+	int size;
+	int uniqueSize;
+
 
 	public SDBTrie(){
-		root = null;
+		root = new Node();
+		pointer = new Node(root);
+		int size = 0;
+		int uniqueSize = 0;
+	}
+	
+	public void addWord(String word){
+		for (int i = 0; i < word.length()-1; i++){
+			word.charAt(i)
+		}
+		if (isUnique){
+
+		}
+		throw UnsupportedOperationException;
+	}
+
+	public boolean isUnique()){
+		throw UnsupportedOperationException;
+	}
+
+	public int size(){
+		throw UnsupportedOperationException;
+	}
+
+	public int UniqueSize(){
+		throw UnsupportedOperationException;
+	}
+
+	public int getDupeNumber(){
+		throw UnsupportedOperationException;
 	}
 
 	class Node {
 		char c;
+		int dupeNumber;
 		Node[] children;
+		Node pointsTo;
 
 		public Node(){
 			c = '';
+			dupeNumber = 0;
+			children = new Node[0];
+			pointsTo = null;
 
 		}
 
@@ -19,5 +61,36 @@ public class SDBTrie{
 			c = a;
 
 		}
+
+		public Node(Node n){
+			pointsTo = n;
+		}
 	}
+
+	public static void main(String[] args) throws IOException {
+        SDBTrie tree = new SDBTrie();
+        Scanner s = null;
+        try {
+            s = new Scanner(System.in);
+            while (s.hasNext()) {
+                String item = s.next(); // Scanner splits input on whitespace, by default
+                tree.add(item);
+            }
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+
+        // Print bag size and distinct contents
+        System.out.format("Total number of words: %d\n", tree.size());
+        System.out.format("Unique number of words: %d\n", tree.uniqueSize());
+
+        /*// Print distinct words in bag and their frequency
+        for (Obliterator i = bag.iterator(); i.hasNext(); ) {
+            Node node = i.next();
+            System.out.format("%s %d\n", node.item, node.count);
+        }
+        */
+    }
 }
--- FunWithTrees/TrieInterface.java
+++ FunWithTrees/TrieInterface.java
@@ -4,5 +4,15 @@ public interface TrieInterface{
 	public void addWord(String word);
 	//adds a word to the prefix tree
 
+	public int size();
+	// number of words added to the Trie
+
+	public int UniqueSize();
+	// number of unique words
+
+	public int getDupeNumber();
+	// number of times word has appeared
+
+
 
 }
