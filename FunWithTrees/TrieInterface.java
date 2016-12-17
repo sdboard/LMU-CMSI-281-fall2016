7f529093d33a1ea721951118d6e2227d4d3607cf
Stephen Board <sboard@lion.lmu.edu>
2016-12-16 19:33:20 -0800

    Update TrieInterface.java

--- FunWithTrees/TrieInterface.java
+++ FunWithTrees/TrieInterface.java
@@ -1,18 +1,9 @@
 //TrieInterface.java
 public interface TrieInterface{
-	//list of methods that my trie will implement
-	public void addTheWord(String word);
-	//adds a word to the prefix tree
-
-	public int size();
-	// number of words added to the Trie
-
-	public int uniqueSize();
-	// number of unique words
-
-	// public int getDupeNumber();
-	// // number of times word has appeared
-
-
-
+    //list of methods that my trie will implement
+    public void add(String word);
+    
+    public int size();
+    
+    public static void toString(Node root)
 }
