ba3ffddda3a7f3b0691539442d105f6156a6c61e
Stephen Board <sboard@lion.lmu.edu>
2016-12-16 19:29:40 -0800

    Super Brute
    So it works! :D It prints out the total number of words, and each word with its frequency.
    I'm sure this is enough to turn in. But i'm going to try to do some sorting to figure out the top ten most used words excluding articles.

--- FunWithTrees/TrieHard.java
+++ FunWithTrees/TrieHard.java
@@ -10,9 +10,8 @@ public class TrieHard implements TrieInterface {
 	int uniqueSize;
 	int level;
 	static char[] chars = new char[0];
-	static char[] helper;
 	static Box[] strings = new Box[0];
-	static Node statictemp;
+	static char[] wordbuilder = new char[1000];
 	
 	static class Box{
 		String word;
@@ -25,7 +24,6 @@ public class TrieHard implements TrieInterface {
 
 
 	public TrieHard(){
-		System.out.println("Makin a Trie");
 		root = new Node();
 		pointer = new Node(root);
 		int size = 0;
@@ -35,7 +33,7 @@ public class TrieHard implements TrieInterface {
 	public void add(String word){
 		String chars = clean(word);
 		if (chars.length() > 0) {
-
+		
 			this.size++;
 			Node traverser = this.root;
 			level = 1;
@@ -48,11 +46,12 @@ public class TrieHard implements TrieInterface {
 					traverser = traverser.children[n];
 			    }else {
 					traverser = traverser.children[n];
+					uniqueSize--;
 			    }
 				level++;
 				if (i == chars.length() - 1){
 						traverser.wordCount++;
-						uniqueSize++;
+						
 			   }
 		   }
 	   }
@@ -72,50 +71,32 @@ public class TrieHard implements TrieInterface {
 
 	public int size(){
 		return size;
-		//throw new UnsupportedOperationException();
 	}
 
 	public int uniqueSize(){
 		return uniqueSize;
-		//throw new UnsupportedOperationException();
 	}
 
-
-	public static void topTen(Node root){
-		//Stack maxStack = new Stack();
-		//maxStack.pu
-		if (root != null){
-			visit(root);
-			for (int i = 0; i < root.children.length; i++){
-				// if(root.children[i] != null) {
-				// 	System.out.println("newChile");
-				// }
-				Node newRoot = root.children[i];
-				topTen(newRoot);
-			}
-		}
-		return;
-	}
-
-	public static void visit(Node n){
-		System.out.format("\n< %s >", n.c);
-		System.out.print(n.wordCount);
-		System.out.print("    " + n.layer);
-   }
 	public static void toString(Node root){
 		triePrinter(root);
 		for(int i = 0; i < strings.length; i++){
-			System.out.println(strings[i].word+" "+ strings[i].t);
+			System.out.println( strings[i].t+" \t |"+strings[i].word);
 	   }
    }
+
 	public static void triePrinter(Node root){
+		wordbuilder[root.layer] = root.c;
 		greenCarpet(root);
 		if(root.wordCount != 0){
-			boxAdd(root.wordCount);
-			int f = (int) chars[0];
-			helper = new char[0];
-			statictemp = root.children[f];
-			arrayHelp(statictemp,f,0);
+			//manually trim the array!!
+			if((int) wordbuilder[root.layer+1] != 0){
+				int y = root.layer+1;
+				while((int) wordbuilder[y] != 0){
+					wordbuilder[y] = (char) 0;
+					y++;
+				}
+			}
+			box2Add(root.wordCount);
 		}
 		
 		for (int i = 0; i < root.children.length; i++){
@@ -127,29 +108,6 @@ public class TrieHard implements TrieInterface {
 		
 		return;
 	}
-	public static void arrayHelp(Node current, int asciLetter, int arrayspot){
-		char[] temp = new char[helper.length+1];
-		for(int i = 0; i < helper.length; i++){
-			temp[i] = helper[i];
-		}
-		temp[helper.length] = chars[helper.length];
-		
-		for(int i = 0; i < 26; i++){
-			//Node n
-		}
-		int k = asciLetter;
-		Node next = current.children[asciLetter];
-		for (int i = 0; i < chars.length; i++){
-			helper[i] = current.c;
-			//k++
-			//int x = (int) chars[i+1]
-			current = current.children[(int) chars[i+1]];
-		}
-		while(current.marked = true){
-			k++;
-			helper[k] = chars[k];
-		}
-	}
 
 	public static void greenCarpet(Node n){
 		char[] temp = new char[chars.length+1];
@@ -160,11 +118,15 @@ public class TrieHard implements TrieInterface {
 		chars = temp;
 	}
 
-	public static void boxAdd(int count){
+	public static void box2Add(int count){
 		Box object = new Box();
 		String wort = "";
-		for (int i = 0; i < chars.length; i++){
-			wort += chars[i];
+		int m = 0;
+		int q = (int) wordbuilder[m];
+		while(q != 0){
+			wort += wordbuilder[m];
+			m++;
+			q = (int) wordbuilder[m];
 		}
 		object.word = wort;
 		object.t = count;
@@ -176,39 +138,6 @@ public class TrieHard implements TrieInterface {
 		 strings = temp;
 	}
 	
-	/*
-	class Stack {
-		class StackNode{
-			StackNode next;
-			int value;
-			public StackNode(StackNode next, int value){
-				this.next = next;
-				this.value = value;
-			}
-		}
-		StackNode top;
-		int max;
-		public Stack() {
-			top = null;
-			n = 0;
-		}
-		public void push(int s) {
-			StackNode newTop = new StackNode(top, s);
-			top = newTop;
-			n++;
-		}
-		public String pop() {
-			StackNode oldTop = top;
-			top = top.next;
-			n--;
-			return oldTop.value;
-		}
-		public String peek() {
-			return top.value;
-		}
-	}
-	*/
-
 	class Node {
 		char c;
 		Node[] children;
@@ -218,7 +147,6 @@ public class TrieHard implements TrieInterface {
 		boolean marked;
 
 		public Node(){
-			System.out.println("Makin a Node");
 			c = ' ';
 			children = new Node[26];
 			specialPointer = null;
@@ -250,19 +178,7 @@ public class TrieHard implements TrieInterface {
 			return true;
 		}
 
-		public Node match(char theChar){
-			for(int i = 0; i < 26; i++){
-				if(this.children[i] != null){
-					if (this.children[i].c == theChar){
-						return this.children[i];
-					}
-				}
-			}
-			return null;
-		}
-
 		public Node(Node n){//iterator node
-			System.out.println("Makin a pointer Node");
 			specialPointer = n;
 		}
 
@@ -270,17 +186,12 @@ public class TrieHard implements TrieInterface {
 
 	public static void main(String[] args) throws IOException {
 		long t1 = System.currentTimeMillis();
-		//cd Desktop/Computer_stuff/cmsi281/FunWithTrees/
-		System.out.println(String.format("And So It Begins"));
 		TrieHard tree = new TrieHard();
-		System.out.println("new scannder");
-        Scanner s = null;
+		Scanner s = null;
         try {
-        	System.out.println("System in it");
-            s = new Scanner(System.in);
+        	s = new Scanner(System.in);
             while (s.hasNext()) {
-            	System.out.println("A word __ at ya");
-                String item = s.next(); // Scanner splits input on whitespace, by default
+            	String item = s.next(); // Scanner splits input on whitespace, by default
                 tree.add(item);
         	}
         } finally {
@@ -288,22 +199,12 @@ public class TrieHard implements TrieInterface {
                 s.close();
             }
         }
-
-        System.out.println("n__a we pm made it");
         // Print bag size and distinct contents
-        System.out.format("Total number of words: %d\n", tree.size());
-        System.out.format("Unique number of words: %d\n", tree.uniqueSize());
-        //System.out.format("The top ten words used: %d\n", tree.topTen());
-        //topTen(tree.root);
-        System.out.println("\n");
+        System.out.format("\nTotal number of words: %d\n\n", tree.size());
+        System.out.println("Frequency|Word");
+        // Print distinct words in bag and their frequency
         toString(tree.root);
         System.out.println("\n");
-		System.out.println(System.currentTimeMillis() - t1);
-        /*// Print distinct words in bag and their frequency
-        for (Obliterator i = bag.iterator(); i.hasNext(); ) {
-            Node node = i.next();
-            System.out.format("%s %d\n", node.item, node.count);
-        }
-        */
+		System.out.println("Runtime: " + (System.currentTimeMillis() - t1) + "ms");
     }
 }
